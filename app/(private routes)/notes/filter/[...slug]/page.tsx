a409952cf9aea51cc98c40892f7df6d6584ae35e
Alina <a.slipenka@gmail.com>
2026-02-08 15:46:43 +0200

    add structures for hw

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -5,7 +5,7 @@ import {
 } from '@tanstack/react-query';
 
 import NotesClient from './Notes.client';
-import { fetchNotes } from '@/lib/api';
+import { fetchNotes } from '@/lib/api/api';
 import type { Metadata } from 'next';
 
 type Props = {
